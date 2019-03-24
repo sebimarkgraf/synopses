41268fec814b21a055af0ebd165ce12fb6a502fb
Jannik Koch <Jannik.Koch@protonmail.com>
2019-03-24 10:16:45 +0100

    swt2: Add to build script

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ files=("Algorithmen 2/algorithmen2.tex"
        "Numerik für Numerik für Informatiker/numerik.tex"
        "Programmierparadigmen/propa.tex"
        "Robotik I/robotik.tex"
-       "Sicherheit/sicherheit.tex")
+       "Sicherheit/sicherheit.tex"
+       "Softwaretechnik 2/softwaretechnik2.tex")
 for f in "${files[@]}"; do
     echo "Changing to $basepath/$(dirname "$f")"
     cd "$basepath/$(dirname "$f")"
